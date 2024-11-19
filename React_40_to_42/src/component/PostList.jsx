9f84f5608b238a8350c07c4f77708d5e0c43c420
Mahavir745 <mahavir777kumar@gmail.com>
2024-11-19 17:34:41 +0530

    new parts has been added, 40 to 46

--- React_40_to_42/src/component/PostList.jsx
+++ React_40_to_42/src/component/PostList.jsx
@@ -9,14 +9,25 @@ const PostList = () => {
 
   const [fetching,setFetching] = useState(false)
 
+ 
+
   useEffect(()=>{
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     setFetching(true)
-      fetch('https://dummyjson.com/posts')
+      fetch('https://dummyjson.com/posts' , {signal})
         .then((res) => res.json())
         .then((data) => {
           addInitialPostList(data.posts);
           setFetching(false)
-        })
+        });
+        
+        return (
+          ()=>{
+            controller.abort()
+          }
+        )
   },[])
 
   return (
